ee38d7bfe4aa32fe7a9cd04f67bd095f1b48e626
anshuman2051 <anshumansingh2051@gmail.com>
2020-05-02 17:25:05 +0530

    completed hooks assignment

---
+++ hooks-3/src/components/Ingredients/Ingredients.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import Search from './Search';
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    setUserIngredients(prevIngredients => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredient }
+    ]);
+  };
+
+  const removeIngredientHandler = id=>{
+    console.log( id);
+    setUserIngredients( prevIngredients=>{
+      const newIng = prevIngredients.filter((ig)=> ig.id !== id);
+      return newIng;
+    })
+  }
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search />
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
+      </section>
+    </div>
+  );
+};
+
+export default Ingredients;
